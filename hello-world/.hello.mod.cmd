8033d59c3060bfc10935b2f384fd3004fff193dd
Soham Biswas <sohambiswas41@gmail.com>
2020-11-25 21:50:50 +0530

    Remove gitignore files

--- hello-world/.hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nibba/OSS/kernel-modules/hello-world/hello.mod := { echo  /home/nibba/OSS/kernel-modules/hello-world/hello.o; echo; } > /home/nibba/OSS/kernel-modules/hello-world/hello.mod
