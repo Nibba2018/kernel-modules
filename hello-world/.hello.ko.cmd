7891b8ff508abe6dc667e9ad6ac91c4c49dcb818
Soham Biswas <sohambiswas41@gmail.com>
2020-11-25 21:47:13 +0530

    add Makefile and README

---
+++ hello-world/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nibba/OSS/kernel-modules/hello-world/hello.ko := ld -r -m elf_x86_64 --build-id=sha1  -T ./scripts/module-common.lds -o /home/nibba/OSS/kernel-modules/hello-world/hello.ko /home/nibba/OSS/kernel-modules/hello-world/hello.o /home/nibba/OSS/kernel-modules/hello-world/hello.mod.o;  true
---
+++ hello-world/.hello.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nibba/OSS/kernel-modules/hello-world/hello.mod := { echo  /home/nibba/OSS/kernel-modules/hello-world/hello.o; echo; } > /home/nibba/OSS/kernel-modules/hello-world/hello.mod
